7138ce18e0264e4d166aea50dfb8de34b8abe982
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-11-17 09:03:03 -0600

    feat: :sparkles: add public/images

--- chatbot-api/src/index.ts
+++ chatbot-api/src/index.ts
@@ -1,10 +1,19 @@
 import express from 'express'
+import path from 'path'
 import { envConfig } from './shared/config/env'
 import { router as chatRouter} from './routes/chat.routes'
 
 const PORT = envConfig.PORT;
 const app = express()
 app.use(express.json())
+
+// get images
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.get('/images/:imageName', (req, res) => {
+    const imageName = req.params.imageName;
+    res.sendFile(path.join(__dirname, '../public', 'images', imageName));
+});
+  
 
 app.use("/api/chat", chatRouter)
 
